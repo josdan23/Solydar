1e9fbd6a95f39caddb13b8843c4473cbdb75d74b
DanielYapura <danielyapura@gmail.com>
2021-01-20 10:23:38 -0300

    first commit

---
+++ src/main/java/com/solydarteam/solydar_server/SolydarServerApplication.java
@@ -0,0 +1,13 @@
+package com.solydarteam.solydar_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolydarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolydarServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solydarteam/solydar_server/SolydarServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solydarteam.solydar_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolydarServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
