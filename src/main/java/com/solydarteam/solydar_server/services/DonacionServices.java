82828a0d2a7332b71c87d459f4a2cc9c1e86f1d7
DanielYapura <danielyapura@gmail.com>
2021-02-25 08:46:08 -0300

    Se optimizo el codigo de DonaciónServices borrando codigo en desuso

--- src/main/java/com/solydarteam/solydar_server/services/DonacionServices.java
+++ src/main/java/com/solydarteam/solydar_server/services/DonacionServices.java
@@ -12,48 +12,15 @@ import java.util.List;
 
 public class DonacionServices {
 
-    /*private Donacion donacion;
-
-    public void iniciarDonacion(Donante donador){
-        donacion = donador.crearDonacion();
-        System.out.println(donacion);
-    }
-
-    public void agregarDonativo(ItemSolicitado donativo, int cantidad){
-        //donacion.agregarDonativo(donativo, cantidad);
-    }
-
-    public void confirmarDonacion(Evento evento){
-        evento.registrarDonacion(donacion);
-    }*/
-
-/*
-    //Todo: el donante lo debería recuperar de la sesion actual
-    public void donarAEvento(Donador donante,
-                             HashMap<ItemSolicitado, Integer> listaDeDonativos,
-                             Evento evento){
-
-        donante.donar(evento, listaDeDonativos);
-    }
-*/
-
+    //Todo: El donanate lo debería recuperar de la sesion actual, es el usuario logeado
+    //Todo: Debería buscar el evento por id, (implementar cuando meta DB)
+    
     public void donarAEvento(Donador donante,
                              List<DetalleDonacion> listaDeDonativos,
                              Evento evento) {
+
         donante.donar(evento, listaDeDonativos);
-    }
 
-    public void donarAEvento(HashMap<Integer, Integer> listaDeDonativos, Integer idEvento) {
-        //Todo: implementar (cuando tenga base de datos)
-        /*
-        tener en cuenta que al evento lo debo recuperar, junto con sus pedidos
-        debo verificar que los items de la donación corresponden con lo que se pide,
-        y el donante es el que crea la donación, lo debo recuperar de la sesion actual.
-         */
-
-        //Donante donante = usuarioLogeado();
-        //Evento eventoElegido = EventoRepositorio.recuperarPorId(idEvento);
-        //donante.donar(eventoElegido, listaDeDonativos);
     }
 
 }
