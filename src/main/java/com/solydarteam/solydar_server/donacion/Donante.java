32d0684e9a065e3dc7960b5e28e18af761ff14d0
DanielYapura <danielyapura@gmail.com>
2021-02-25 08:36:48 -0300

    Se borro el codigo anterior

--- src/main/java/com/solydarteam/solydar_server/donacion/Donante.java
+++ src/main/java/com/solydarteam/solydar_server/donacion/Donante.java
@@ -5,64 +5,32 @@ import com.solydarteam.solydar_server.evento.Evento;
 import java.util.List;
 
 public class Donante implements Donador {
-    private String nombre = "Daniel es el donante";
+    private String nombre = "Daniel";
 
     @Override
     public String toString() {
         return String.format("Donante: %s", nombre);
     }
 
-    //LISTA DE DONACIÓN ES UN HASHMAP
-   /* public void donar(Evento evento, HashMap<ItemSolicitado, Integer> listaDeDonativos){
 
-        *//*
-            Crea una donación, agrega la lista de donativos confirmada y luego
-            registra las donaciones en el evento.
-         *//*
+    //LISTA DE DONACIÓN ES UN ARRAYLIST
+    @Override
+    public void donar(Evento evento, List<DetalleDonacion> listaDeDonativos) {
+
         try {
-            Donacion donacion = new Donacion(this);
+            if (evento.getPedidoSolicitado().esListaDeDonacionesConfirmada(listaDeDonativos)) {
 
-            List<DetalleDonacion> listaDeDonativosConfirmada =
-                    getListaDeDonativosConfirmada(evento, listaDeDonativos);
+                Donacion nuevaDonacion = new Donacion(this);
+                nuevaDonacion.setListaDonativos(listaDeDonativos);
+                evento.registrarDonacion(nuevaDonacion);
 
-            donacion.setListaDonativos(listaDeDonativosConfirmada);
-
-            evento.registrarDonacion(donacion);
+            }
+            else
+                System.out.println("Hay donaciones que no corresponden a lo que se pide");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
-*/
-    //LISTA DE DONACIÓN ES UN ARRAYLIST
-    @Override
-    public void donar(Evento evento, List<DetalleDonacion> listaDeDonativos) {
-
-        if (evento.getPedidoSolicitado().esListaDeDonacionesConfirmada(listaDeDonativos)){
-            try {
-                Donacion nuevaDonacion = new Donacion(this);
-                nuevaDonacion.setListaDonativos(listaDeDonativos);
-                evento.registrarDonacion(nuevaDonacion);
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        else
-            System.out.println("Hay donaciones que no corresponden a lo que se pide");
-    }
-
-   /* private List<DetalleDonacion> getListaDeDonativosConfirmada(Evento evento, HashMap<ItemSolicitado, Integer> listaDeDonativos) {
-        List<DetalleDonacion> detalleDonacionsList = new ArrayList<>();
-
-        //Recorrer cada item de la lista de donativos
-        for(Map.Entry<ItemSolicitado, Integer> unItemDeLaListaDonada : listaDeDonativos.entrySet()) {
-
-            if (evento.getPedidoSolicitado().estaEnLaLista(unItemDeLaListaDonada.getKey())){
-                detalleDonacionsList.add(new DetalleDonacion(unItemDeLaListaDonada.getKey(), unItemDeLaListaDonada.getValue()));
-            }
-        }
-        return detalleDonacionsList;
-    }*/
 
 }
